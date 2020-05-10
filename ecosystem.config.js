2c01c947c78a25ede49ab1691cf7b6b467415b8b
TecHunter <a.baschenis@gmail.com>
2020-05-10 12:14:06 +0200

    add ecosystem config for pm2

---
+++ ecosystem.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  apps : [{
+    script: 'bot.js',
+    watch: '.',
+    // cwd: 'D:\\_projects\\starborne-js\\discord',
+    // interpreter: '.\\node_modules\\@babel\\node\\bin\\babel-node.js'
+    interpreter: 'babel-node'
+  }],
+
+  deploy : {
+    production : {
+      user : 'SSH_USERNAME',
+      host : 'SSH_HOSTMACHINE',
+      ref  : 'origin/master',
+      repo : 'git@github.com:TecHunter/discord-starborne-js.git',
+      path : '/opt/discord-starborne-bot',
+      'pre-deploy-local': '',
+      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production',
+      'pre-setup': ''
+    }
+  }
+};
